cc1145c764e37ceebfc00423372fb14853d48f96
John Flynn <JJimmyFlynn@gmail.com>
2020-04-03 11:10:47 -0400

    Reorganize homepage, js cleanup

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,7 @@
 import '../styles/main.scss'
 
-import Vue from 'vue'
 import '../../node_modules/lazysizes/plugins/respimg/ls.respimg'
 import 'lazysizes'
 
 import './modules/work-slider'
 import './modules/descriptor-list'
-
-// eslint-disable-next-line
-new Vue({
-  el: '#app'
-})
--- assets/scripts/modules/work-slider.js
+++ assets/scripts/modules/work-slider.js
@@ -1,50 +1,41 @@
 import Flickity from 'flickity'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const workSlider = document.querySelectorAll('[data-slider]')
-  const workSliderSelect = document.querySelector('[data-work-slider-select]')
-  const workSliderSelectTabs = workSliderSelect.querySelectorAll('[data-tab-select-tab]')
+const workSlider = document.querySelectorAll('[data-slider]')
+const workSliderSelect = document.querySelector('[data-work-slider-select]')
+const workSliderSelectTabs = workSliderSelect.querySelectorAll('[data-tab-select-tab]')
 
-  /**
-   * Exit if no slider is present
-   */
-  if (!workSlider) {
-    return
-  }
-
-  /**
-   * Initialize slider for each portfolio category
-   */
-  workSlider.forEach(slider => {
-    // eslint-disable-next-line
-    new Flickity(slider, {
-      pageDots: true,
-      prevNextButtons: false,
-      draggable: false
-    })
+/**
+ * Initialize slider for each portfolio category
+ */
+workSlider.forEach(slider => {
+  // eslint-disable-next-line
+  new Flickity(slider, {
+    pageDots: true,
+    prevNextButtons: false,
+    draggable: false
   })
+})
 
-  /**
-   * Swap slider types on tab clicks
-   */
-  workSliderSelectTabs.forEach(tab => {
-    tab.addEventListener('click', function () {
-      const sliderIndex = this.getAttribute('data-slider-index')
-      const activeTab = document.querySelector('.c-tab-select__tab--active')
-      const activeSlider = document.querySelector('.m-work__slider--active')
-      const targetSlider = document.querySelector(`.m-work__slider[data-slide-index='${sliderIndex}']`)
+/**
+ * Swap slider types on tab clicks
+ */
+workSliderSelectTabs.forEach(tab => {
+  tab.addEventListener('click', function () {
+    const sliderIndex = this.getAttribute('data-slider-index')
+    const activeTab = document.querySelector('.c-tab-select__tab--active')
+    const activeSlider = document.querySelector('.m-work__slider--active')
+    const targetSlider = document.querySelector(`.m-work__slider[data-slide-index='${sliderIndex}']`)
 
-      /**
-       * Update active tab
-       */
-      activeTab.classList.remove('c-tab-select__tab--active')
-      this.classList.add('c-tab-select__tab--active')
+    /**
+     * Update active tab
+     */
+    activeTab.classList.remove('c-tab-select__tab--active')
+    this.classList.add('c-tab-select__tab--active')
 
-      /**
-        * Update active slider
-        */
-      activeSlider.classList.remove('m-work__slider--active')
-      targetSlider.classList.add('m-work__slider--active')
-    })
+    /**
+      * Update active slider
+      */
+    activeSlider.classList.remove('m-work__slider--active')
+    targetSlider.classList.add('m-work__slider--active')
   })
 })
